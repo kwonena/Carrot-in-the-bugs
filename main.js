f36ba2e35b477c279a975f6f7aa4d0f1ee4cc5f2
KwonEna <eeena214@gmail.com>
2021-09-26 20:21:00 +0900

    Fix : Timer 오류 해결 및 완성 & 되돌아가기 기능 구현

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 const play = document.querySelector('.play_btn');
+const reply = document.querySelector('.reply_btn');
 const ten = document.querySelector('.ten');
+const carrot = document.querySelectorAll('.carrot');
+const bug = document.querySelectorAll('bug');
+const carrotCounter = document.querySelector('.carrot_counter');
 
 let time = 0;
-let count = 10;
+let count = 2; // test 2sec
 
 play.addEventListener('click', () => {
-    // start 아이콘을 pause 아이콘으로 변경
     clearInterval(time);
     time = setInterval("startTimer()", 1000);
+    play.innerHTML = `<i class="fas fa-pause"></i>`;
+
+    // 1. 당근 모두 클릭시 성공
+
+    // 2. 시간 초과시 실패
+    // 3. 벌레 선택시 실패
+});
+
+reply.addEventListener('click', () => {
+    play.disabled = false;
+    play.innerHTML = `<i class="fas fa-play"></i>`;
+
+    // 타이머 초기화
+    ten.innerHTML = `10`;
+    count = 2;
 });
 
 function startTimer() {
@@ -17,8 +35,6 @@ function startTimer() {
     if(count <= 0) {
         clearInterval(time);
         alert('시간초과');
-        return;
+        play.disabled = true;
     }
 }
-
-// play버튼 재클릭시 count가 -1이 되는게 출력되어 보여지는 오류
