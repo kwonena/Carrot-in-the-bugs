4c9aa721c3d92fca36caaa5dc67ab7e64ae9e0c4
KwonEna <eeena214@gmail.com>
2021-09-29 23:58:59 +0900

    Feat : 랜덤 이미지 배치 함수 생성

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const play = document.querySelector('.play_btn');
 const reply = document.querySelector('.reply_btn');
 const ten = document.querySelector('.ten');
 const carrot = document.querySelectorAll('.carrot');
-const bug = document.querySelectorAll('bug');
+const bug = document.querySelectorAll('.bug');
 const carrotCounter = document.querySelector('.carrot_counter');
+const imgAll = document.querySelectorAll('img');
 
 let time = 0;
 let count = 2; // test 2sec
@@ -13,10 +14,12 @@ play.addEventListener('click', () => {
     time = setInterval("startTimer()", 1000);
     play.innerHTML = `<i class="fas fa-pause"></i>`;
 
-    // 1. 당근 모두 클릭시 성공
+    // 모든 이미지 랜덤 배치
+    showImage();
+    // 당근 모두 클릭시 성공
 
-    // 2. 시간 초과시 실패
-    // 3. 벌레 선택시 실패
+    // 시간 초과시 실패
+    // 벌레 선택시 실패
 });
 
 reply.addEventListener('click', () => {
@@ -37,4 +40,12 @@ function startTimer() {
         alert('시간초과');
         play.disabled = true;
     }
+}
+
+function showImage() {
+    imgAll.style.position = 'absolute';
+    imgAll.style.top = document.body.clientHeight * Math.random() + 'px';
+    imgAll.style.left = document.body.clientWidth * Math.random() + 'px';
+    document.body.appendChild(imgAll);
+
 }
